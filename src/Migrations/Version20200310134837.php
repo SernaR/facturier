f4903c7c1a65f719dab00edc245493b3722cd0b1
SernaR <rserna@outlook.fr>
2020-11-21 14:20:37 +0100

    heroku setup

--- src/Migrations/Version20200310134837.php
+++ src/Migrations/Version20200310134837.php
@@ -22,7 +22,8 @@ final class Version20200310134837 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE user ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE user ADD roles JSON NOT NULL');
+        //$this->addSql('ALTER TABLE user ADD roles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\'');
     }
 
     public function down(Schema $schema) : void
